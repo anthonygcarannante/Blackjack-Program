062fba60a5fbccca636f3c00021cea65b8b84a6b
carannantea <79670978+carannantea@users.noreply.github.com>
2022-01-21 19:11:09 -0500

    Blackjack with aces as 1 or 11

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,4 @@
-import random
+import random, csv
 
 ### BlackJack Game ###
 
@@ -21,31 +21,39 @@ while len(dealer_cards) != 2:
 while len(player_cards) != 2:
     player_cards.append(random.randint(1, 11))
     if len(player_cards) == 2:
-        print(f"You: {player_cards[0]}, {player_cards[1]}")
+        print(f"You:    {player_cards[0]}, {player_cards[1]}")
 
 # Sum of the Player Cards
-while sum(player_cards) < 21:
-    action_taken = str(input("Do you want to hit or stay?  "))
-    if action_taken == "hit":
-        player_cards.append(random.randint(1,11))
-        print(f"You: {str(sum(player_cards))}, {player_cards} ")
-    else:
-        print(f"You: {str(sum(player_cards))}, {player_cards} ")
-        print(f"Dealer: {str(sum(dealer_cards))}, {dealer_cards} ")
-
+while sum(player_cards) <= 21:
+    if sum(player_cards) == 21:
         break
+    else:    
+        action_taken = str(input("Do you want to hit or stay?  "))
+        if action_taken == "hit":
+            player_cards.append(random.randint(1,11))
+            # if player has at least 11 and hits an ace, then the ace counts as 1
+            if player_cards[-1] == 11 and sum(player_cards) > 10:
+                player_cards[-1] -= 10
+            print(f"You:    {str(sum(player_cards))}, {player_cards} ")
+        else:
+            print(f"You:    {str(sum(player_cards))}, {player_cards} ")
+            print(f"Dealer: {str(sum(dealer_cards))}, {dealer_cards} ")
+            break
 
 # Sum of the Dealer Cards
 while sum(dealer_cards) <= 16 and sum(player_cards) <= 21:
-    print(f"Dealer hits with {str(sum(dealer_cards))} ")
+    print(f"Dealer HITS")
     dealer_cards.append(random.randint(1,11))
-    print(f"Dealer: {str(sum(dealer_cards))} {dealer_cards} ")
+    # if the dealer has at least 11 and hits an ace, then the ace counts as 1
+    if dealer_cards[-1] == 11 and sum(dealer_cards) > 10:
+        dealer_cards[-1] -= 10
+    print(f"Dealer: {str(sum(dealer_cards))}, {dealer_cards} ")
 
 # Player and Dealer Hand Comparisons 
 if sum(player_cards) == 21 and len(player_cards) == 2:
     print(f"BLACKJACK! You WIN!")
 elif sum(player_cards) > 21:
-    print(f"Dealer: {str(sum(dealer_cards))} and {dealer_cards} ")
+    print(f"Dealer: {str(sum(dealer_cards))}, {dealer_cards} ")
     print("You BUSTED!")
     print("Dealer WINS!")
 elif sum(player_cards) <= 21 and sum(dealer_cards) > 21:
@@ -62,6 +70,9 @@ else:
     print("Nothing Happened")
 
 print("-----------------------------------")
+
+# Store results in CSV file for analysis
+
 
 # Compare the sums of the dealer and player cards
 # If P sum is greater than 21 = BUST
