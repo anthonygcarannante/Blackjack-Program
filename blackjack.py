cc0ee3a2bc5cefd40d4ba06da6ab4607b7213753
carannantea <79670978+carannantea@users.noreply.github.com>
2022-01-21 22:48:28 -0500

    Added data tracking to pandas dataframe (needs work)

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 from card_programs import dealing_cards, dealer_action, player_action
 
 ### BlackJack Game ###
@@ -7,12 +8,17 @@ from card_programs import dealing_cards, dealer_action, player_action
 cards = [2,3,4,5,6,7,8,9,10,10,10,10,11]
 
 play = "yes"
-bankroll = 500
+money = 500
+hand_list = []
+bankroll = []
+player_stats = {"Hand": [], "Cards": []}
+dealer_stats = {"Hand": [], "Cards": []}
 
-while play == "yes" and bankroll > 0:
+while play == "yes" and money > 0:
     print("-----------------------------------")
-    print(f"Bankroll: ${bankroll}")
-
+    print(f"Bankroll: ${money}")
+    # Track amount of money before the hand started
+   
     # Dealer Cards
     dealer_cards = []
 
@@ -35,28 +41,28 @@ while play == "yes" and bankroll > 0:
     # If the player hits blackjack (i.e., 21 on their first two cards)
     if sum(player_cards) == 21 and len(player_cards) == 2:
         print(f"BLACKJACK! You WIN!")
-        bankroll += bet
+        money += bet * 3/2
     # If the player busts, or gets a hand over 21
     elif sum(player_cards) > 21:
         print(f"Dealer: {str(sum(dealer_cards))}, {dealer_cards} ")
         print("You BUSTED!")
         print("Dealer WINS!")
-        bankroll -= bet
+        money -= bet
     # If the player is under 21 and the dealer busts
     elif sum(player_cards) <= 21 and sum(dealer_cards) > 21:
         print("Dealer BUSTED!")
         print("You WIN!")
-        bankroll += bet
+        money += bet
     # If neither the player nor the dealer bust
     elif sum(player_cards) <= 21 and sum(dealer_cards) <= 21:
         # If the player's hand is higher, player wins
         if sum(player_cards) > sum(dealer_cards):
             print(f"You WIN!")
-            bankroll += bet
+            money += bet
         # If the dealer's hand is higher, dealer wins
         elif sum(player_cards) < sum(dealer_cards):
             print(f"Dealer WINS!")
-            bankroll -= bet
+            money -= bet
         # If the player and dealers hands are the same
         else:
             print(f"PUSH")
@@ -65,8 +71,20 @@ while play == "yes" and bankroll > 0:
 
     print("-----------------------------------")
 
+    # Compile stats
+    player_stats["Hand"].append(sum(player_cards))
+    player_stats["Cards"].append(player_cards)
+    dealer_stats["Hand"].append(sum(dealer_cards))
+    dealer_stats["Cards"].append(dealer_cards)
+    bankroll.append(money)
+
     play = str(input("Would you like to deal another hand? (yes/no)  "))
 
 print("-----------------------------------")
-print(f"New Balance: ${bankroll}")
+print(f"New Balance: ${money}")
 print("-----------------------------------")
+
+data = [[player_stats["Hand"],player_stats["Cards"],dealer_stats["Hand"],dealer_stats["Cards"],bankroll]]
+blackjack_stats_df = pd.DataFrame(data, columns=['Player Hand','Player Cards','Dealer Hands','Dealer Cards','Bankroll'])
+
+blackjack_stats_df.to_csv('blackjack_data.csv')
